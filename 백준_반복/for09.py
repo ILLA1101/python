d3e554cfbb6bdc491a7d07a1f5a8eac70fd9e184
ILLA1101 <mj061101k@naver.com>
2022-11-16 21:56:22 +0900

    .

--- 백준_반복/for09.py
+++ 백준_반복/for09.py
@@ -1,8 +1,15 @@
-n = int(input())
+n = int(input()) #줄 입력
 
-for i in range(n) :
-    for j in range(n-(i+1)) :
-        print(' ',end = '')
-    for j in range(i+1) :
-        print('*',end = '')
-    print()
+for i in range(n) : #줄
+    for j in range(n-(i+1)) : #빈칸을 채움
+        print(' ',end = '') # 빈칸을 채우면서 ,end = ' '  < 를 사용하여 줄바꿈을 안함
+    for j in range(i+1) : # 별을 출력
+        print('*',end = '') # '*'을 출력 하면서 ,end = ' ' < 를 사용하여 줄바꿈을 안함
+    print() # 줄바꿈
+'''
+0000*
+000**
+00***
+0****
+*****
+'''
