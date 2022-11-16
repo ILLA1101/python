6e770f65142e812833553e57c62fc91a772b69f9
ILLA1101 <mj061101k@naver.com>
2022-11-16 17:34:36 +0900

    .

---
+++ 백준(반복)/for04.py
@@ -0,0 +1,12 @@
+x = int(input())
+n = int(input())
+
+count = 0
+for i in range(n) :
+    a, b = map(int, input().split())
+    count += a * b
+
+if x == count :
+    print('Yes')
+else :
+    print('No')
